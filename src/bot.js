e5c27b98d8c142d330f94637b0e0c630c423afb9
saurabh meena <2019kucp1085@iiitkota.ac.in>
2021-03-27 10:38:12 +0530

    first commit

--- src/bot.js
+++ src/bot.js
@@ -28,7 +28,9 @@ client.on('message',(message)=>{
         if(message.content === 'bc')
         message.channel.send("nothing just chill");
         if(message.content === 'wah')
-        message.channel.send("chal hatt bosdike");        
+        message.channel.send("chal hatt bosdike");   
+        if(message.content === 'Hello')
+        message.channel.send("hello ky bosdike kam kr apna");        
 
 
 
